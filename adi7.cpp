20ae3d23a7831d037703f614fec1e63743d6e153
SABHYAGUPTA5XR <sabhyagupta4xr@gmail.com>
2024-09-15 18:19:42 +0530

    Modified

--- adi7.cpp
+++ adi7.cpp
@@ -1,11 +1,7 @@
 #include <iostream> 
 using namespace std; 
-inline int cube(int s) 
-{ 
-return s*s*s; 
-} 
 int main()
-{ 
-cout << "The cube of 3 is: " << cube(3) << "\n"; 
-return 0; 
+{
+    cout<< "This is the modification from my vscode"<<endl;
+    return 0;
 }
